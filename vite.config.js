3ec928ba9f825a855fe1297cb3cead08bf3013b0
IvanG <kikin-gr2000@hotmail.com>
2022-12-06 18:46:16 -0600

    final ahora si 4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [ react() ],
-  base: "https://IvanGonzalezR.github.io/reactjs_state_machine"
+  base: "https://IvanGonzalezR.github.io/reactjs_state_machine/"
 })
